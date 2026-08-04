6f7a505f5e8d8c62e8f0614213dd46291ab5835c
dmitry.golofast <dmitry.golofast@gmail.com>
2026-08-04 11:56:10 +0300

    fix(rebrand): change title

--- src/app/(frontend)/(auth)/auth-card.tsx
+++ src/app/(frontend)/(auth)/auth-card.tsx
@@ -24,7 +24,7 @@ export function AuthCard({
           href="/"
           className="mb-6 flex items-center justify-center font-semibold tracking-tight"
         >
-          Taskona
+          eventFit App
         </Link>
         <Card>
           <CardHeader className="text-center">
